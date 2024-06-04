498cce33495e9af90dae83f6ca0f21472a217135
AnniePhan02 <a5phan@ucsd.edu>
2024-06-03 21:32:34 -0700

    what i have so far

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -4,69 +4,35 @@ url = sys.argv[1]
 
 # Your code to modify url goes here
 
-# # Parse the URL to extract the token and parameters
-# parsed_url = urllib.parse.urlparse(url)
-# query_params = urllib.parse.parse_qs(parsed_url.query)
+parsedUrl = urllib.parse.urlparse(url)
+# print(parsedUrl)
+hash_old = parsedUrl.query[6:38] # token value
+# print(hash_old)
+command_old = parsedUrl.query[39:]
+# print(len(command_old))
 
-# # Extract the token
-# token = query_params['token'][0]
+# token = list(urllib.parse.parse_qs(url).values())[0][0] # with this i can get the token value of the url
+# # print(token)
+m = hash_old + command_old
+# m = token + "&user=earlence&command1=ListSquirrels&command2=NoOp" # need a way to somehow parse the message
 
-# # Reconstruct the original message without the token part
-# original_message = parsed_url.query.split('&', 1)[1]
-
-# # Calculate the length of the original message including the password
-# # The password length is 8 characters
-# original_message_length = 8 + len(original_message)
-
-# # Calculate the padding for the original message
-# padding_str = padding(original_message_length * 8)
-
-# # Create a new md5 object with the given token as the initial state
-# h = md5(state=bytes.fromhex(token), count=(original_message_length + len(padding_str)) * 8)
-
-# # Update the hash with the new data to append
-# append_data = '&command3=UnlockAllSafes'
-# h.update(append_data)
-
-# # Generate the new token
-# new_token = h.hexdigest()
-
-# # Construct the new URL
-# new_message = original_message + padding_str.decode('latin1') + append_data
-# new_url = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}?token={new_token}&{urllib.parse.quote(new_message)}"
-
-# print(new_url)
-
-# print(query_params['token'])
+# get the current hash
+bits = (len(m) + len(padding(len(m) * 8))) * 8
+h = md5(state=bytes.fromhex(m), count=bits) 
+# print(h.hexdigest())
 
 
+# ## need to calculate the padding length
 
 
+x = "&command3=UnlockAllSafes" # appended value
+
+result = m + urllib.parse.quote(padding(len(m)*8))+ x
+# print(result)
+
+h.update(result)
+# print(h.hexdigest()) # new token value
 
 
-# ------------ MY PSEUDOCODE --------
-
-
-token = list(urllib.parse.parse_qs(url).values())[0][0] # with this i can get the token value of the url
-# print(token)
-
-# get current state of h
-h = md5(state=bytes.fromhex(token), count=512) # is the count correct? Need to check
-print(h.hexdigest())
-
-
-## need to calculate the padding length
-
-
-appended_value_x = "&command3=UnlockAllSafes"
-
-newMsg = m.encode("utf-8") + padding(len(m)*8) + appended_value_x.encode("utf-8")
-
-
-h.update(newMsg)
-print(h.hexdigest()) # MD5 hash should be the same
-
-new_token = h.hexdigest()
-
-new_url= oldUrlParsed + token + appended_value_x
+new_url= "http://bank.cse127.ucsd.edu/pa5/api?token=" + h.hexdigest() + command_old
 print(new_url)
