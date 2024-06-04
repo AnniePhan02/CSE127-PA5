6742ee069cb54af7662672f4239110b13492430e
Melina Dimitropoulou <mdimitropoulouka@ucsd.edu>
2024-06-04 12:49:49 -0700

    solution for 2b

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -5,36 +5,25 @@ url = sys.argv[1]
 parsedUrl = urllib.parse.urlparse(url)
 # print(parsedUrl)
 hash_old = parsedUrl.query[6:38] # token value
-# print(hash_old)
+#print(hash_old)
 command_old = parsedUrl.query[39:]
-# print(command_old)
-
-# token = list(urllib.parse.parse_qs(url).values())[0][0] # with this i can get the token value of the url
-# print(token)
-m = hash_old + command_old
-# m = token + "&user=earlence&command1=ListSquirrels&command2=NoOp" # need a way to somehow parse the message
 
 # # get the current hash
-# bits = (len(m) + len(padding(len(m) * 8))) * 8
-bits = (len(m) + len(padding(len(m) * 8))) * 8
+old_len = len("AAAAAAAA" + command_old)
+bits = (old_len + len(padding(old_len * 8))) * 8
 h = md5(state=bytes.fromhex(hash_old), count=bits) 
-# h = md5(m)
-# h.update(m)
-# print(h.hexdigest())
 
 
 # need to calculate the padding length
-
-
 x = "&command3=UnlockAllSafes" # appended value
+h.update(x)
 
-result = m + urllib.parse.quote(padding(len(m)*8))+ x
+result = command_old + urllib.parse.quote(padding(old_len*8))+ x
 
 # print(result)
 
-h.update(result)
 # print(h.hexdigest()) # new token value
 
 
-new_url= "http://bank.cse127.ucsd.edu/pa5/api?token=" + h.hexdigest() + command_old
+new_url= "http://bank.cse127.ucsd.edu/pa5/api?token=" + h.hexdigest() + "&" + result
 print(new_url)
